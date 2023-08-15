b091a8f4a877b7da6e1c811738af2ccd6b4d9977
Benjamin Tan <benjamin.tan@osim.com.sg>
2023-08-15 16:08:52 +0800

    todo_crud

---
+++ elixir/ch4/todo_crud.ex
@@ -0,0 +1,47 @@
+defmodule TodoList do
+  defstruct next_id: 1, entries: %{}
+
+  @type id :: integer
+  @type entry :: map
+  @type entries :: %{id => entry}
+
+  @spec new() :: TodoList
+  def new(), do: %TodoList{}
+
+  @spec add_entry(TodoList, entry) :: TodoList
+  def add_entry(todo_list, entry) do
+    entry_with_id = Map.put(entry, :id, todo_list.next_id)
+
+    put_in(
+      todo_list,
+      [Access.key(:entries), todo_list.next_id],
+      entry_with_id
+    )
+    |> update_in([Access.key(:next_id)], &(&1 + 1))
+  end
+
+  @spec entries(TodoList, Date) :: [entry]
+  def entries(todo_list, date) do
+    for {_id, entry} <- todo_list.entries,
+        entry.date == date do
+      entry
+    end
+  end
+
+  @spec update_entry(TodoList, id, (entry -> entry)) :: TodoList
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        put_in(todo_list, [Access.key(:entries), new_entry.id], new_entry)
+    end
+  end
+
+  @spec delete_entry(TodoList, id) :: TodoList
+  def delete_entry(todo_list, id) do
+    pop_in(todo_list, [Access.key(:entries), id])
+  end
+end
